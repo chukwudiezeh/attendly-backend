a6764ef67e8f02a62ba966ad509e4a36cf7721d6
Chukwudi Ezeh <chukwudi.ezeh@konga.com>
2025-09-15 00:51:30 +0100

    class chores

--- src/routes/classRoutes.js
+++ src/routes/classRoutes.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const ClassController = require('../controllers/classController');
+const { validateToken } = require('../middlewares/tokenValidator');
 
 // Create a class
-router.post('/', ClassController.createClass);
+router.post('/', validateToken, ClassController.createClass);
 
 // Update a class
-router.put('/:id', ClassController.updateClass);
+router.put('/:id', validateToken, ClassController.updateClass);
 
 // Delete a class
 // router.delete('/:id', ClassController.deleteClass);
 
 // Get a single class
-router.get('/:id', ClassController.getClass);
+router.get('/:id', validateToken, ClassController.getClass);
 
 // Get all classes by course
-router.get('/curriculum-course/:courseId', ClassController.getClassesByCourse);
+router.get('/curriculum-course/:courseId', validateToken, ClassController.getClassesByCourse);
 
 // Get all classes
 // router.get('/', ClassController.getAllClasses);
